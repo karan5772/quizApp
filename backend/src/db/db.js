09c0f42cee26389499e6182d975c8f11d9c2753b
karan5772 <karankumar8239@gmail.com>
2026-01-09 01:32:45 +0530

    improved db connection

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,15 +3,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = () => {
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-      console.log("Connected to mongodb ✅");
-    })
-    .catch((err) => {
-      console.log("Error connecting to mongodb ❌", err);
-    });
+const db = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    await mongoose.connection.db.admin().command({ ping: 1 });
+
+    console.log("MongoDB fully connected ✅");
+  } catch (err) {
+    console.error("MongoDB connection FAILED ❌");
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 export default db;
